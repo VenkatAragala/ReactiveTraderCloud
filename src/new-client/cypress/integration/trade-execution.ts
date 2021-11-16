c40e47944d740c99ed096a189f2f4eee5795044d
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-11-16 15:19:19 +0000

    feat(new-client): Added price tick test

--- src/new-client/cypress/integration/trade-execution.ts
+++ src/new-client/cypress/integration/trade-execution.ts
@@ -63,6 +63,20 @@ describe("Trade Execution", () => {
     expect(true).to.equal(true)
   })
 
+  it("Should tick price", () => {
+    getTile(symbol)
+      .find('[direction="Sell"]')
+      .then(($button) => {
+        const val = $button.text().replace("SELL", "")
+        getTile(symbol)
+          .find('[direction="Sell"]')
+          .should(($button) => {
+            const newVal = $button.text().replace("SELL", "")
+            expect(newVal).not.to.eq(val)
+          })
+      })
+  })
+
   it("Should format notional input", () => {
     getTile(symbol).find("input").type("500000").should("have.value", "500,000")
   })
@@ -200,5 +214,92 @@ describe("Trade Execution", () => {
         getTile(symbol).contains("Initiate RFQ")
       })
     })
+  })
+})
+
+describe("Live Rates", () => {
+  before(() => {
+    cy.visit("http://localhost:1917")
+  })
+
+  it("Should show loader", () => {
+    cy.get('[data-testid="live-rates-loader"]')
+  })
+
+  it("Should show all currency pairs", () => {
+    cy.contains("Live Rates")
+    cy.get('[data-testid^="tile-"]').should("have.length", 9)
+  })
+
+  it("Should show tiles in analytics view", () => {
+    cy.get('[data-testid="toggleButton"]').should(
+      "have.attr",
+      "data-qa-id",
+      "workspace-view-Analytics",
+    )
+    cy.get('[data-testid="historical-graph"]').should("have.length", 9)
+  })
+
+  describe("When toggle view button", () => {
+    it("Should show tiles in normal view", () => {
+      cy.get('[data-testid="toggleButton"]')
+        .click()
+        .should("have.attr", "data-qa-id", "workspace-view-Normal")
+      cy.get('[data-testid="historical-graph"]').should("have.length", 0)
+    })
+  })
+
+  describe("Currency navigation", () => {
+    ;["EUR", "USD", "GBP", "AUD", "NZD"].forEach((currency) => {
+      describe(`When ${currency} is selected`, () => {
+        before(() => {
+          cy.get(`[data-testid="menuButton-${currency}"]`).click()
+        })
+
+        it(`Should show only ${currency} pairs`, () => {
+          cy.get('[data-testid^="tile-"]').each((tile) => {
+            expect(tile.text()).to.contain(currency)
+          })
+        })
+      })
+    })
+
+    describe(`When ALL is selected`, () => {
+      before(() => {
+        cy.get(`[data-testid="menuButton-Symbol(all)"]`).click()
+      })
+
+      it("Should show all currency pairs", () => {
+        cy.get('[data-testid^="tile-"]').should("have.length", 9)
+      })
+    })
+  })
+
+  describe("Price ticking", () => {
+    ;[
+      "EURUSD",
+      "USDJPY",
+      "GBPUSD",
+      "GBPJPY",
+      "EURJPY",
+      "AUDUSD",
+      "NZDUSD",
+      "EURCAD",
+      "EURAUD",
+    ].forEach((symbol) =>
+      it(`Should tick ${symbol} price`, () => {
+        getTile(symbol)
+          .find('[direction="Sell"]')
+          .then(($button) => {
+            const val = $button.text().replace("SELL", "")
+            getTile(symbol)
+              .find('[direction="Sell"]')
+              .should(($button) => {
+                const newVal = $button.text().replace("SELL", "")
+                expect(newVal).not.to.eq(val)
+              })
+          })
+      }),
+    )
   })
 })
