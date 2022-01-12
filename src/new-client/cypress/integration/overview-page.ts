7a9a36f79541ad19dd5b8135b686604e54951a71
VenkatAragala <venkat.aragala@weareadaptive.com>
2022-01-12 14:23:28 -0800

    Removed CSS selectors

--- src/new-client/cypress/integration/overview-page.ts
+++ src/new-client/cypress/integration/overview-page.ts
@@ -1,22 +1,28 @@
 describe("Trades", () => {
   before(() => {
-    cy.visit("http://localhost:1917")
+    cy.visit("http://localhost:1919")
   })
 
   it("Should be able to click on Adaptive Icon which navigates to the Adaptive hoempage", () => {
-    cy.get(".sc-daBunf > .sc-eKYRIR").should("be.visible").click()
+    cy.get('[data-qa="header__root-logo"]').should("be.visible").click()
   })
 
-  it("Should be able to toggle lightmode", () => {
-    cy.get(".sc-kHWWYL > svg").should("be.visible").click()
-    cy.wait(3000)
-  })
-
-  it("Should be able to toggle darkmode", () => {
-    cy.get(".sc-hOPeYd").should("be.visible").click()
+  it("Should be able to toggle lightmode/Darkmode", () => {
+    cy.get("div").find('[width="18"]').should("be.visible").click()
   })
 
   it("Should be able to click on Get in touch button at the bottom", () => {
-    cy.get(":nth-child(2) > .sc-EZqKI").should("be.visible").dblclick()
+    cy.get('[data-qa="contact-us-button"]').should("be.visible").dblclick()
+  })
+
+  it("detect theme", () => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)")
+    darkThemeMq.addListener((e) => {
+      if (e.matches) {
+        // Theme set to dark.
+      } else {
+        // Theme set to light.
+      }
+    })
   })
 })
--- src/new-client/cypress/integration/trades.ts
+++ src/new-client/cypress/integration/trades.ts
@@ -1,6 +1,16 @@
 describe("Trades", () => {
   before(() => {
-    cy.visit("http://localhost:1917")
+    cy.visit("http://localhost:1919")
+    //cy.visit("https://web.uat.reactivetrader.com/")
+  })
+
+  it("Should verify elements the number of elements in the blotter", () => {
+    cy.get("table")
+      .find("tbody")
+      .then((tbody) => {
+        const listingCount = Cypress.$(tbody).length
+        expect(tbody).to.have.length(listingCount)
+      })
   })
 
   var keyWords = [
@@ -21,125 +31,260 @@ describe("Trades", () => {
   ]
   var randomItem = keyWords[Math.floor(Math.random() * keyWords.length)]
 
+  var words = [
+    "Equals",
+    "Not equal",
+    "Less than",
+    "Less than or equals",
+    "Greater than",
+    "Greater than or equals",
+    "In range",
+  ]
+  var option = words[Math.floor(Math.random() * words.length)]
+
+  var filterWord = ["Done", "Rejected", "Select All"]
+  var word = filterWord[Math.floor(Math.random() * filterWord.length)]
+
+  var wordFilter = ["Buy", "Sell", "Select All"]
+  var filters = wordFilter[Math.floor(Math.random() * wordFilter.length)]
+
+  var wordOption = [
+    "Select All",
+    "EURUSD",
+    "USDJPY",
+    "GBPUSD",
+    "GBPJPY",
+    "EURJPY",
+    "AUDUSD",
+    "NZDUSD",
+    "EURCAD",
+    "EURAUD",
+  ]
+  var wordFilters = wordOption[Math.floor(Math.random() * wordOption.length)]
+
+  var wordsOption = [
+    "Select All",
+    "USD",
+    "JPY",
+    "AUD",
+    "NZD",
+    "CAD",
+    "GBP",
+    "EUR",
+  ]
+  var randomFilters =
+    wordsOption[Math.floor(Math.random() * wordsOption.length)]
+
   it("Should search trades using the filter", () => {
-    cy.get(".sc-igOmtu").type(randomItem)
-    cy.wait(1000)
-    cy.get(".sc-gGuRsA > svg > path").click()
+    cy.get('[data-qa="quick-filer__filter-input"]').type(randomItem)
+    cy.get("table")
+      .find("tbody")
+      .then((tbody) => {
+        const listingCount = Cypress.$(tbody).length
+        expect(tbody).to.have.length(listingCount)
+      })
   })
 
   it("Should download .CSV file", () => {
     cy.get(".svg-size").click()
+    //TODO --- Check the line below amd if it downloads the file remve the line above
+    //cy.get('[aria-label="Export to CSV"]')
   })
 
   it("should download .CSV file when filter is applied", () => {
-    cy.get(".sc-igOmtu").type(randomItem)
+    cy.get('[data-qa="quick-filer__filter-input"]').type(randomItem)
     cy.wait(2000)
     cy.get(".svg-size").click()
   })
 
   it("Should be able to cancel the search filter by clicking on 'X'", () => {
-    cy.get(".sc-gGuRsA > svg > path").click()
+    cy.get('[data-qa="quick-filter__filter-clear-icon"]').click()
   })
 
   describe("Check filter checkboxes", () => {
-    var words = [
-      "Equals",
-      "Not equal",
-      "Less than",
-      "Less than or equals",
-      "Greater than",
-      "Greater than or equals",
-      "In range",
-    ]
-    var option = words[Math.floor(Math.random() * words.length)]
-
-    it("Should be able to click on checkbox filter for Trade ID column", () => {
-      cy.get(".hWSVxc > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.get(".sc-JsfZP").select(option)
+    it("Should check if the blotter has any trades done", () => {
+      cy.get("tbody").children("tr").should("have.length.greaterThan", 0)
     })
 
-    var filterWord = ["Done", "Rejected"]
-    var word = filterWord[Math.floor(Math.random() * filterWord.length)]
+    describe("Check Trade ID column", () => {
+      it("Should find the Trade ID column from the blotter", () => {
+        cy.get("thead").find('[width="100"]')
+        cy.contains("Trade ID")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+      })
 
-    it("should be able to click on checkbox filter in the dropdown for Status column", () => {
-      //checking if cypress can locate the element
-      cy.get(":nth-child(3) > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.get(".sc-hYRTwp").type(word)
-      cy.contains("Done").dblclick({ force: true })
-      cy.contains("Rejected").dblclick({ force: true })
-      cy.contains("Select All").dblclick({ force: true })
-      cy.get(":nth-child(3) > .sc-hkoqWr").contains("Status").click()
+      it("Should be able to set the filter for Trade ID column", () => {
+        cy.get('[aria-label="Filter trades by Trade ID field value"]')
+          .get("select")
+          .select(option)
+          .should("not.be.disabled")
+      })
+
+      it("Should search by typing text using the filter instead of selecting drop down for Trade ID column", () => {
+        //TODO
+        cy.get('[aria-label="Primary filter value"]')
+      })
     })
 
-    it("Should be able to click on checkbox filter in the dropdown for Trade date column", () => {
-      //Checking if cypress can detect the elements
-      cy.get(".kckzpb > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.contains("Equals").click({ force: true })
+    describe("Check Status column", () => {
+      it("Should find the Status column from the blotter", () => {
+        cy.get("thead").find('[width="110"]')
+        cy.contains("Status")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .click()
+
+        cy.contains(word).dblclick({ force: true })
+      })
     })
 
-    it("Should be able to click on checkbox filter for Direction column", () => {
-      cy.get(":nth-child(5) > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.contains("Buy").dblclick({ force: true })
-      cy.wait(1000)
-      cy.contains("Sell").dblclick({ force: true })
-      cy.wait(1000)
-      cy.get(":nth-child(5) > .sc-hkoqWr").click()
+    describe("Check Trade Date column", () => {
+      it("Should find the Trade Date column from the blotter", () => {
+        cy.get("thead").find('[width="130"]')
+        cy.contains("Trade Date")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+      })
+
+      it("Should be able to set the filter for Trade ID column", () => {
+        cy.get('[aria-label="Open Trade Date field filter pop up"]')
+          .get("select")
+          .should("not.be.disabled")
+          .select(option)
+      })
     })
 
-    it("Should be able to click on checkbox filter for CCYCCY column", () => {
-      cy.get(":nth-child(6) > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.contains("GBPUSD").dblclick({ force: true })
-      cy.contains("GBPJPY").dblclick({ force: true })
-      cy.contains("AUDUSD").dblclick({ force: true })
-      cy.contains("USDJPY").dblclick({ force: true })
-      cy.contains("EURUSD").dblclick({ force: true })
-      cy.contains("EURJPY").dblclick({ force: true })
+    describe("Check Direction column", () => {
+      it("Should find the Direction column from the blotter", () => {
+        cy.get("thead").find('[width="110"]')
+        cy.contains("Direction")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+
+        cy.contains(filters).dblclick({ force: true })
+      })
     })
 
-    it("Should be able to click on checkbox filter for DEALCCY column", () => {
-      cy.get(".uQWbs > .sc-hkoqWr > :nth-child(2)").should("be.visible").click()
-      cy.get(".bLqgfz").dblclick({ force: true })
-      cy.get(".sc-bUQyIj > :nth-child(3)").dblclick({ force: true })
-      cy.get(".sc-bUQyIj > :nth-child(4)").dblclick({ force: true })
-      cy.get(".sc-bUQyIj > :nth-child(5)").dblclick({ force: true })
-      cy.get(".sc-bUQyIj > :nth-child(6)").dblclick({ force: true })
-      cy.get(".sc-bUQyIj > :nth-child(7)").dblclick({ force: true })
-      cy.get(".uQWbs > .sc-hkoqWr").click()
+    describe("Check CCYCCY column", () => {
+      it("Should find the CCYCCY column from the blotter", () => {
+        cy.get("thead").find('[width="110"]')
+        cy.contains("CCYCCY")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+
+        cy.contains(wordFilters).dblclick({ force: true })
+      })
     })
 
-    it("Should be able to click on checkbox filter for Notional Column", () => {
-      cy.get(".jEQuGk > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.get(".sc-JsfZP").select(option)
+    describe("Check Deal CCY column", () => {
+      it("Should find the Deal CCY column from the blotter", () => {
+        cy.get("thead").find('[width="90"]')
+        cy.contains("Deal CCY")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+
+        cy.contains(randomFilters).dblclick({ force: true })
+      })
     })
 
-    it("Should be able to click on checkbox filter for Rate column", () => {
-      cy.get(".eqsjEG > .sc-hkoqWr > :nth-child(2)")
-        .should("be.visible")
-        .click()
-      cy.get(".sc-JsfZP").select(option)
+    describe("Check Notional column", () => {
+      it("Should find the Notional column from the blotter", () => {
+        cy.get("thead").find('[width="120"]')
+        cy.contains("Notional")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+      })
+
+      it("Should be able to set the filter for Notional column", () => {
+        cy.get('[aria-label="Open Notional field filter pop up"]')
+          .get("select")
+          .select(option)
+          .should("not.be.disabled")
+      })
     })
 
-    it("Should be able to click on checkbox filter for Value Date column", () => {
-      cy.get(".gIxNWS > .sc-hkoqWr > :nth-child(2)").click()
-      cy.get(".sc-JsfZP").select(option)
+    describe("Check Rate column", () => {
+      it("Should find the Rate column from the blotter", () => {
+        cy.get("thead")
+          .find('[width="100"]')
+          .eq(1)
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+      })
+
+      it("Should be able to set the filter for Rate column", () => {
+        cy.get('[aria-label="Filter trades by Rate field value"]')
+          .get("select")
+          .select(option)
+          .should("not.be.disabled")
+      })
     })
 
-    it("Should be able to click on checkbox filter for Trader column", () => {
-      cy.get(":nth-child(11) > .sc-hkoqWr > :nth-child(2)").click()
-      cy.contains("Select All").dblclick({ force: true })
-      cy.contains("JPW").dblclick({ force: true })
+    describe("Check Value Date column", () => {
+      it("Should find the Value Date column from the blotter", () => {
+        cy.get("thead").find('[width="120"]')
+        cy.contains("Value Date")
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .click()
+        //.should('be.visible').should("not.be.disabled").click()
+      })
+
+      it("Should be able to set the filter for Value Date column", () => {
+        cy.get('[aria-label="Open Value Date field filter pop up"]')
+          .get("select")
+          .select(option)
+          .should("not.be.disabled")
+      })
+    })
+
+    describe("Check Trader column", () => {
+      it("Should find the Trader column from the blotter", () => {
+        cy.get("thead")
+          .find('[width="110"]')
+          .eq(3)
+          .invoke("show")
+          .find('[aria-hidden="true"]')
+          .eq(1)
+          .should("be.visible")
+          .should("not.be.disabled")
+          .click()
+
+        if (cy.contains("JPW")) {
+          cy.contains("JPW").should("not.be.disabled").dblclick({ force: true })
+        }
+      })
     })
   })
 })
